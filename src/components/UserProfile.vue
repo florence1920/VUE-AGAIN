9901b79147e3d3c3f90a7b3a991cd186a6a2863c
florence1920 <jetaime12121@gmail.com>
2021-08-02 22:15:50 +0900

    css

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="box_user">
         <slot name="username"></slot>
         <slot name="karma"></slot>
         <slot name="time"></slot>
@@ -14,6 +14,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.box_user {overflow: hidden; width: 450px; margin:10px; background: #212529; padding: 25px 40px;}
+.box_user .icon {float: left; width: 20%; margin: 0 5% 0 0; color:#fff; font-size:70px;}
+.box_user .name {float: left; margin:15px 0 0; color:#fff; font-size:23px; font-weight: 600; text-decoration:none;}
+.box_user .time {float: left; width: 75%; color:#777;}
 </style>
